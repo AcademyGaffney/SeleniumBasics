ee5356d94857ba64cc17eb48dfed320a609bbe58
AcademyGaffney <michaelkgaffney@gmail.com>
2020-10-02 11:07:51 -0500

    m,

---
+++ selenium_basics/src/test/java/selenium_basics/TestSuite.java
@@ -0,0 +1,14 @@
+package selenium_basics;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SuiteDisplayName("OurDayFourTests")
+@SelectClasses({AssertionIntro.class, LoginTests.class })
+public class TestSuite {
+
+}
