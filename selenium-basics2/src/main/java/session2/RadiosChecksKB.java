07fdb7c1577eaec3150b7ccec675a14c232f6e6c
AcademyGaffney <michaelkgaffney@gmail.com>
2020-12-22 10:18:37 -0600

    2a

--- selenium-basics2/src/main/java/session2/RadiosChecksKB.java
+++ selenium-basics2/src/main/java/session2/RadiosChecksKB.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class RadiosChecksKB {
 
 	public static void main(String[] args) throws InterruptedException {
-		//System.setProperty("webdriver.chrome.driver", "chromedriver");
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
 		WebDriver driver = new ChromeDriver();
 
 		String baseURL = "http://demo.guru99.com/test/radio.html";
